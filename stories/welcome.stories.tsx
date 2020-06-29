bfedf0f37b6d30aba1780ed80f37617eb09bcf6f
kimizuy <kimizuyamasaki@gmail.com>
2020-06-30 00:19:45 +0900

    add storybook

---
+++ stories/welcome.stories.tsx
@@ -0,0 +1,6 @@
+import { linkTo } from "@storybook/addon-links"
+import { Welcome } from "@storybook/react/demo"
+
+export default { title: "Welcome" }
+
+export const toStorybook = () => <Welcome showApp={linkTo("Button")} />
