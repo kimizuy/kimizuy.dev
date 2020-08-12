9fe333bd61f09d4b7d85901a2d79b2f5c1004362
kimizuy <kimizuyamasaki@gmail.com>
2020-08-12 17:07:42 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from 'next/head'
 import Layout from '@/components/layout'
-import utilStyles from '@/styles/utils.module.css'
-import Link from 'next/link'
-import Date from '@/components/date'
 import getAllPostPreviews from '@/lib/getAllPostPreviews'
 import { Preview } from '@/types/post'
 import { SITETITLE } from '@/lib/constants'
-import Previews from '@/components/previews'
+import CardList from '@/components/cardList'
 
 const previews: Preview[] = getAllPostPreviews()
 
@@ -16,14 +13,8 @@ export default function Home() {
       <Head>
         <title>{SITETITLE}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          <a href="https://github.com/kimizuy">kimizuy</a> のブログです。
-        </p>
-      </section>
-      <section className={`${utilStyles.padding1px}`}>
-        <h2>Blog</h2>
-        <Previews previews={previews} />
+      <section>
+        <CardList previews={previews} />
       </section>
     </Layout>
   )
