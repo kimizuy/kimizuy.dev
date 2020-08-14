462c0177a63537b6451d381740d5109244b25738
kimizuy <kimizuyamasaki@gmail.com>
2020-08-14 16:53:59 +0900

    update

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -1,47 +1,38 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import Date from '@/components/date'
 import Layout from '@/components/layout'
-import { getAllTags, getSelectedTagData } from 'lib/tags'
-import utilStyles from 'styles/utils.module.css'
-import { Meta } from '@/types/post'
+import { Preview } from '@/types/post'
+import getAllTags from '@/lib/getAllTags'
+import getSelectedTagPreviews from '@/lib/getSelectedTagPreviews'
+import CardList from '@/components/cardList'
 
 type Props = {
   selectedTag: string
-  selectedTagPostsData: (Meta & { id: string })[]
+  selectedTagPreviews: Preview[]
 }
 
-export default function Tag({ selectedTag, selectedTagPostsData }: Props) {
+export default function Tag({ selectedTag, selectedTagPreviews }: Props) {
   return (
     <Layout>
       <Head>
         <title>{selectedTag}</title>
       </Head>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>{selectedTag}</h2>
-        <ul className={utilStyles.list}>
-          {selectedTagPostsData.map(({ id, date, title, tag }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-              <br />
-              <small>{typeof tag === 'string' ? tag : tag?.join(', ')}</small>
-            </li>
-          ))}
-        </ul>
+      <section>
+        <CardList previews={selectedTagPreviews} />
       </section>
     </Layout>
   )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllTags()
+  const paths = getAllTags().map((tag) => {
+    return {
+      params: {
+        tag,
+      },
+    }
+  })
+
   return {
     paths,
     fallback: false,
@@ -49,12 +40,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const selectedTag = params?.tag as string
-  const selectedTagPostsData = getSelectedTagData(selectedTag)
+  const selectedTag = params.tag as string
+  const selectedTagPreviews = getSelectedTagPreviews(selectedTag)
   return {
     props: {
       selectedTag,
-      selectedTagPostsData,
+      selectedTagPreviews,
     },
   }
 }
