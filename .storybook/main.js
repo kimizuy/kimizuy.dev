21c9f2da158f0184cd9526151628d835235203c5
kimizuy <kimizuyamasaki@gmail.com>
2020-09-03 02:13:37 +0900

    delete storybook

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-const path = require("path")
-
-module.exports = {
-  stories: ["../stories/**/*.stories.js", "../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
-  presets: [path.resolve(__dirname, "./next-preset.js")],
-}
