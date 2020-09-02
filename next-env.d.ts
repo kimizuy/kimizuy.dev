a6e93f12dd11e8ec36d7e0066542e2da89c19e76
kimizuy <kimizuyamasaki@gmail.com>
2020-09-03 02:13:17 +0900

    update

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,50 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// image
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.png'
+declare module '*.svg'
+declare module '*.ico'
+declare module '*.webp'
+declare module '*.jp2'
+
+// mdx
+declare module '@mdx-js/react' {
+  import * as React from 'react'
+  type ComponentType =
+    | 'a'
+    | 'blockquote'
+    | 'code'
+    | 'delete'
+    | 'em'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'hr'
+    | 'img'
+    | 'inlineCode'
+    | 'li'
+    | 'ol'
+    | 'p'
+    | 'pre'
+    | 'strong'
+    | 'sup'
+    | 'table'
+    | 'td'
+    | 'thematicBreak'
+    | 'tr'
+    | 'ul'
+  export type Components = {
+    [key in ComponentType]?: React.ComponentType<{ children: React.ReactNode }>
+  }
+  export interface MDXProviderProps {
+    children: React.ReactNode
+    components: Components
+  }
+  export class MDXProvider extends React.Component<MDXProviderProps> {}
+}
