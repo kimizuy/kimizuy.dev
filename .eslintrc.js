05d87f05ea8b43d2fe4bcd8187e731dd64f32790
kimizuy <kimizuyamasaki@gmail.com>
2020-09-15 23:32:53 +0900

    update eslint prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,48 @@
 module.exports = {
-  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'airbnb-typescript',
+    // Uncomment the following lines to enable eslint-config-prettier
+    // Is not enabled right now to avoid issues with the Next.js repo
+    // 'prettier',
+    // 'prettier/@typescript-eslint',
+  ],
+  // parserOptions: {
+  //   project: './tsconfig.json',
+  // },
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    '@typescript-eslint/semi': 0,
+  },
 }
