716a1239b28bd193fadc5833ecf265f2a576e047
kimizuy <kimizuyamasaki@gmail.com>
2020-10-08 03:07:37 +0900

    add rss

---
+++ scripts/build-rss.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import RSS from 'rss'
+import getAllPostPreviews from '../src/lib/getAllPostPreviews'
+
+const feed = new RSS({
+  title: 'Blog – kimizuy',
+  site_url: 'https://blog.kimizuy.dev',
+  feed_url: 'https://blog.kimizuy.dev/feed.xml',
+})
+
+getAllPostPreviews().forEach(({ link, meta }) => {
+  feed.item({
+    title: meta.title,
+    guid: link,
+    url: `https://blog.kimizuy.dev${link}`,
+    date: meta.date.published,
+    description: meta.description,
+  })
+})
+
+fs.writeFileSync('./out/feed.xml', feed.xml({ indent: true }))
