5bf364ece34d48f4d7f5fc609df5df7d6a8106b8
kimizuy <kimizuyamasaki@gmail.com>
2020-10-23 18:23:43 +0900

    update

--- scripts/build-rss.js
+++ scripts/build-rss.js
@@ -1,23 +1,47 @@
 import fs from 'fs'
+import ReactDOMServer from 'react-dom/server'
 import RSS from 'rss'
-import getAllPostPreviews from '../src/lib/getAllPostPreviews'
+import { dateSortDesc } from '../src/lib/utils'
 
-const feed = new RSS({
-  title: 'Blog – kimizuy',
-  site_url: 'https://blog.kimizuy.dev',
-  feed_url: 'https://blog.kimizuy.dev/feed.xml',
-})
+const importAll = (r) => {
+  return r.keys().map((fileName) => ({
+    link: `/posts${fileName.substr(1).replace(/\/index\.mdx$/, '')}`,
+    module: r(fileName),
+  }))
+}
 
-getAllPostPreviews().forEach(({ link, meta }) => {
-  feed.item({
-    title: meta.title,
-    guid: link,
-    url: `https://blog.kimizuy.dev${link}`,
-    date: meta.date.published,
-    description: meta.description,
+const getAllPostPreviews = async () => {
+  return importAll(
+    require.context('../src/pages/posts/', true, /\.mdx$/)
+  ).sort((a, b) =>
+    dateSortDesc(a.module.meta.date.published, b.module.meta.date.published)
+  )
+}
+
+const generate = async () => {
+  const previews = await getAllPostPreviews()
+
+  const feed = new RSS({
+    title: 'Blog – kimizuy',
+    site_url: 'https://blog.kimizuy.dev',
+    feed_url: 'https://blog.kimizuy.dev/feed.xml',
   })
-})
 
-const rss = feed.xml({ indent: true })
+  previews.forEach(({ link, module: { default: Content, meta } }) => {
+    const html = ReactDOMServer.renderToStaticMarkup(<Content />)
 
-fs.writeFileSync('./.next/static/feed.xml', rss)
+    feed.item({
+      title: meta.title,
+      guid: link,
+      url: `https://blog.kimizuy.dev${link}`,
+      date: meta.date.published,
+      description: html,
+    })
+  })
+
+  const rss = feed.xml({ indent: true })
+
+  fs.writeFileSync('./.next/static/feed.xml', rss)
+}
+
+generate()
