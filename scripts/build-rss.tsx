6915a51ea3dca61d34349d097925c098d40f6859
kimizuy <kimizuyamasaki@gmail.com>
2020-10-24 12:07:57 +0900

    update

--- scripts/build-rss.tsx
+++ scripts/build-rss.tsx
@@ -3,36 +3,46 @@ import { Feed } from 'feed'
 import ReactDOMServer from 'react-dom/server'
 import { dateSortDesc } from '@/lib/utils'
 import { Meta } from '@/types/post'
-import mdx from '@mdx-js/mdx'
 import path from 'path'
 
 export type Preview = { link: string; module: { default: any; meta: Meta } }
 
-const importAll = (r) => {
-  return r.keys().map((fileName) => ({
-    link: `/posts${fileName.substr(1).replace(/\/index\.mdx$/, '')}`,
-    module: r(fileName),
-  }))
-}
+// const importAll = (r) => {
+//   return r.keys().map((fileName) => ({
+//     link: `/posts${fileName.substr(1).replace(/\/index\.mdx$/, '')}`,
+//     module: r(fileName),
+//   }))
+// }
 
-const getAllPostPreviews = (): Preview[] => {
-  return importAll(
-    require.context('../src/pages/posts/', true, /\.mdx$/)
-  ).sort((a, b) =>
-    dateSortDesc(a.module.meta.date.published, b.module.meta.date.published)
-  )
-}
+// const getAllPostPreviews = (): Preview[] => {
+//   return importAll(
+//     require.context('../src/pages/posts/', true, /\.mdx$/)
+//   ).sort((a, b) =>
+//     dateSortDesc(a.module.meta.date.published, b.module.meta.date.published)
+//   )
+// }
 
-const getallposts = () => {
+const getAllPostPreviews = async () => {
   const directory = path.join(process.cwd(), 'src', 'pages', 'posts')
   const files = fs.readdirSync(directory)
 
-  files.forEach((v) => console.log(v))
-}
-getallposts()
+  const entries: { default: any; meta: Meta }[] = await Promise.all(
+    files.map((file) => import(`../src/pages/posts/${file}`))
+  )
 
-const generate = () => {
-  const previews = getAllPostPreviews()
+  const posts = entries.map(({ default: Content, meta }, i) => ({
+    link: `/posts/${files[i]}`,
+    Content,
+    meta,
+  }))
+
+  return posts.sort((a, b) =>
+    dateSortDesc(a.meta.date.published, b.meta.date.published)
+  )
+}
+
+const generate = async () => {
+  const previews = await getAllPostPreviews()
 
   const feed = new Feed({
     title: 'Blog – kimizuy',
@@ -42,15 +52,15 @@ const generate = () => {
     feedLinks: 'https://blog.kimizuy.dev/feed.xml',
   })
 
-  previews.forEach(async ({ link, module: { default: Content, meta } }) => {
-    // const Jsx = await mdx(Content)
+  previews.forEach(({ link, Content, meta }) => {
+    const html = ReactDOMServer.renderToStaticMarkup(<Content />)
 
     feed.addItem({
       title: meta.title,
       id: link,
       link: `https://blog.kimizuy.dev${link}`,
       date: new Date(meta.date.published),
-      description: meta.description,
+      description: html,
     })
   })
 
