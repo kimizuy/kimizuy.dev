b0ce8334474c59d503c05b8c27bc777e029bf414
kimizuy <kimizuyamasaki@gmail.com>
2021-01-03 23:05:34 +0900

    feat: add image overlay

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import '@/styles/global.css'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
-import { ThemeProvider } from '@/lib/ThemeProvider'
+import { ThemeProvider } from '@/providers/themeProvider'
+import { ImageOverlayProvider } from '@/providers/imageOverlayProvider'
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <div>
+    <>
       <Head>
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </Head>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </div>
+      <ImageOverlayProvider>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ImageOverlayProvider>
+    </>
   )
 }
 
