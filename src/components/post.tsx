6007ffc5bd2b75abdfa1b784bafe3f6029af85cf
kimizuy <kimizuyamasaki@gmail.com>
2021-01-07 17:16:20 +0900

    chore

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -43,23 +43,23 @@ const Post: React.VFC<{
   const router = useRouter()
 
   return (
-    <div style={{ position: 'relative', minHeight: '100vh' }}>
+    <>
+      <Head>
+        <title>{meta.title} – kimizuy blog</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@kimizuy" />
+        <meta name="twitter:creator" content="@kimizuy" />
+        <meta name="twitter:title" content={`${meta.title} – kimizuy blog`} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={`${SITE_URL}${meta.image}`} />
+        <meta property="og:url" content={`${SITE_URL}${router.pathname}`} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={`${meta.title} – kimizuy blog`} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={`${SITE_URL}${meta.image}`} />
+        <meta name="description" content={meta.description}></meta>
+      </Head>
       <Layout>
-        <Head>
-          <title>{meta.title} – kimizuy blog</title>
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@kimizuy" />
-          <meta name="twitter:creator" content="@kimizuy" />
-          <meta name="twitter:title" content={`${meta.title} – kimizuy blog`} />
-          <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={`${SITE_URL}${meta.image}`} />
-          <meta property="og:url" content={`${SITE_URL}${router.pathname}`} />
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content={`${meta.title} – kimizuy blog`} />
-          <meta property="og:description" content={meta.description} />
-          <meta property="og:image" content={`${SITE_URL}${meta.image}`} />
-          <meta name="description" content={meta.description}></meta>
-        </Head>
         <article className={styles.container}>
           <h1 className={styles.headingXl}>{meta.title}</h1>
           <LightText>
@@ -71,9 +71,9 @@ const Post: React.VFC<{
           </LightText>
           <MDXProvider components={mdxComponents}>{children}</MDXProvider>
         </article>
+        <ImageOverlay />
       </Layout>
-      <ImageOverlay />
-    </div>
+    </>
   )
 }
 
