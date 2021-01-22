ff4608df02a441def43e3987dc065bcf5a3622fb
kimizuy <kimizuyamasaki@gmail.com>
2021-01-22 11:53:56 +0900

    add file name label

--- src/components/codeBlock.tsx
+++ src/components/codeBlock.tsx
@@ -1,40 +1,45 @@
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/vsDark'
 import React from 'react'
 import styles from './codeBlock.module.css'
-import theme from 'prism-react-renderer/themes/vsDark'
 
 const CodeBlock: React.VFC<{
   children: string
   className: string
 }> = ({ children, className }) => {
-  const language = className.replace(/language-/, '') as Language
+  const splited = className.replace(/language-/, '').split(':')
+  const language = splited[0] as Language
+  const fileName = splited[splited.length - 1]
 
   return (
-    <Highlight
-      {...defaultProps}
-      theme={theme}
-      code={children.trim()}
-      language={language}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={`${className} ${styles.pre}`} style={style}>
-          {tokens.map((line, i) => (
-            <div
-              key={i}
-              {...getLineProps({ line, key: i })}
-              className={styles.line}
-            >
-              <span className={styles.lineNo}>{i + 1}</span>
-              <span className={styles.lineContent}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </span>
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+    <div className={styles.container}>
+      <span className={styles.fileName}>{fileName}</span>
+      <Highlight
+        {...defaultProps}
+        theme={theme}
+        code={children.trim()}
+        language={language}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={`${className} ${styles.pre}`} style={style}>
+            {tokens.map((line, i) => (
+              <div
+                key={i}
+                {...getLineProps({ line, key: i })}
+                className={styles.line}
+              >
+                <span className={styles.lineNo}>{i + 1}</span>
+                <span className={styles.lineContent}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </span>
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   )
 }
 
