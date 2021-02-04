78a862b1fe7fe42bb72e1a70a345368acc2dc3a1
kimizuy <kimizuyamasaki@gmail.com>
2021-02-04 16:42:16 +0900

    refactor: use <button>
    instead of <input type="checkbox">

--- src/components/themeSwitch.tsx
+++ src/components/themeSwitch.tsx
@@ -1,21 +1,46 @@
 import { useTheme } from '@/providers/themeProvider'
+import { useEffect, useState } from 'react'
 import styles from './themeSwitch.module.css'
 
 export const ThemeSwitch: React.VFC = () => {
   const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), [])
 
   return (
-    <div className={styles.switchContainer}>
-      <label className={styles.switch}>
-        Theme Switch
-        <input
-          type="checkbox"
-          checked={theme !== 'dark'}
-          onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          className={styles.slider}
-        />
-        <span className={`${styles.slider} ${styles.round}`} />
-      </label>
-    </div>
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      className={styles.button}
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {mounted && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          stroke="currentColor"
+          className={styles.svg}
+        >
+          {theme === 'dark' ? (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          ) : (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          )}
+        </svg>
+      )}
+    </button>
   )
 }
