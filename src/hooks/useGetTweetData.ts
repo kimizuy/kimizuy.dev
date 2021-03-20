c090f0d1e2f56a6979d4653bcc19d7a9c6364c43
kimizuy <kimizuyamasaki@gmail.com>
2021-03-20 14:20:41 +0900

    refactor: rename

--- src/hooks/useGetTweetData.ts
+++ src/hooks/useGetTweetData.ts
@@ -3,14 +3,14 @@ import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-export const useGetTweetData = (props: {
+export const useGetTweetData = (initialData: {
   tweetId: string
   tweetAst: string
 }): { data: { tweetId: string; tweetAst: string }; error: Error } => {
   const router = useRouter()
   const { basePath } = router
   const url = basePath + '/api/tweet'
-  const { data, error } = useSWR(url, fetcher, { initialData: props })
+  const { data, error } = useSWR(url, fetcher, { initialData })
 
   return { data, error }
 }
