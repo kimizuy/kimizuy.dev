7f68a47d4479b98dea2b6e34a16108c638d9060b
kimizuy <kimizuyamasaki@gmail.com>
2021-12-05 01:43:16 +0900

    remove unused code

--- next.config.js
+++ next.config.js
@@ -6,12 +6,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // @ts-ignore
   images: {
     domains: ['pbs.twimg.com'],
   },
-
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
 
   webpack: (config, options) => {
     // Replace React with Preact only in client production build
