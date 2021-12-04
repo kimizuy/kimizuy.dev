6ae3abf1ead89a6b74441fc567c3a9dfa45858c1
kimizuy <kimizuyamasaki@gmail.com>
2021-12-05 02:25:38 +0900

    fix link tag for rss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,24 +57,12 @@ const App = ({ Component, pageProps }: MyAppProps) => {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        {/* <link
+        <link
           rel="alternate"
           type="application/rss+xml"
           title="RSS 2.0"
           href="/feed.xml"
         />
-        <link
-          rel="alternate"
-          type="application/atom+xml"
-          title="Atom 1.0"
-          href="/atom.xml"
-        />
-        <link
-          rel="alternate"
-          type="application/json"
-          title="JSON Feed"
-          href="/feed.json"
-        /> */}
       </Head>
       <ImageOverlayProvider>
         <ThemeProvider>
