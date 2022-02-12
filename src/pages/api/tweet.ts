7140c5a88785ef8abf3506a4d4c95a4628b7d310
kimizuy <kimizuyamasaki@gmail.com>
2022-02-12 18:24:29 +0900

    comment out fetchTweet() for now

--- src/pages/api/tweet.ts
+++ src/pages/api/tweet.ts
@@ -1,12 +1,13 @@
+/* eslint-disable unused-imports/no-unused-vars */
 import { NextApiRequest, NextApiResponse } from 'next'
-import { fetchTweet } from '@/lib/fetchTweet'
+// import { fetchTweet } from '@/lib/fetchTweet'
 
 const tweetHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const tweet = await fetchTweet()
-  res.status(200).json(tweet)
+  // const tweet = await fetchTweet()
+  // res.status(200).json(tweet)
 }
 
 // eslint-disable-next-line import/no-default-export
