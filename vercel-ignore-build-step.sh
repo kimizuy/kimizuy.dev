55d5b9484740bbf43f7a87c03d83b92949524d92
kimizuy <kimizuyamasaki@gmail.com>
2022-06-12 19:38:02 +0900

    add main

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]  || [[ $VERCEL_GIT_COMMIT_REF == "develop" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
