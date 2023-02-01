94c617e5fa2eb74617c8fff073bef72509174e3b
kimizuy <kimizuyamasaki@gmail.com>
2023-02-02 00:58:34 +0900

    use "@font-face" instead of "@next/fonts"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
-import { Zen_Kaku_Gothic_New } from "@next/font/google";
 import { PropsWithChildren } from "react";
 import { RootLayout } from "../components/RootLayout";
 import "../styles/globals.css";
 import "../styles/reset.css";
 
-const zenKakuGothicNew = Zen_Kaku_Gothic_New({
-  weight: ["400", "700"],
-  style: ["normal"],
-});
+// FIXME: @next/fonts/google not working
+// const zenKakuGothicNew = Zen_Kaku_Gothic_New({
+//   weight: ["400", "700"],
+//   style: ["normal"],
+//   subsets: ["latin"],
+// fallback: [
+//   "Hiragino Kaku Gothic ProN",
+//   "Hiragino Sans",
+//   "Meiryo",
+//   "sans-serif",
+// ],
+// });
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <html lang="ja" className={zenKakuGothicNew.className}>
+    <html lang="ja">
       <head />
       <body>
         <RootLayout>{children}</RootLayout>
