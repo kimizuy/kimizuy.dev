ed630d16be79d8bcbcb629d9ad2c25c5f172a9c0
kimizuy <kimizuyamasaki@gmail.com>
2023-02-02 00:35:40 +0900

    use "@next/font/google"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,12 @@
-import localFont from "@next/font/local";
+import { Zen_Kaku_Gothic_New } from "@next/font/google";
 import { PropsWithChildren } from "react";
 import { RootLayout } from "../components/RootLayout";
 import "../styles/globals.css";
 import "../styles/reset.css";
 
-// Font files can be colocated inside of `app`
-const zenKakuGothicNew = localFont({
-  src: [
-    {
-      path: "../../public/fonts/zen-kaku-gothic-new-v7-japanese/zen-kaku-gothic-new-v7-japanese-regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/zen-kaku-gothic-new-v7-japanese/zen-kaku-gothic-new-v7-japanese-700.woff2",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  display: "swap",
-  fallback: [
-    "Hiragino Kaku Gothic ProN",
-    "Hiragino Sans",
-    "Meiryo",
-    "sans-serif",
-  ],
+const zenKakuGothicNew = Zen_Kaku_Gothic_New({
+  weight: ["400", "700"],
+  style: ["normal"],
 });
 
 export default function Layout({ children }: PropsWithChildren) {
