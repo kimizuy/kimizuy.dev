1a851a75082fd10a4ce477d160fe3f825e2eea96
kimizuy <kimizuyamasaki@gmail.com>
2023-02-15 13:43:32 +0900

    add deepl-node

---
+++ src/libs/deepl.ts
@@ -0,0 +1,12 @@
+import * as deepl from "deepl-node";
+
+const translator = new deepl.Translator(process.env.DEEPL_API_KEY || "");
+
+type TranslateTextParameter = Parameters<deepl.Translator["translateText"]>[0];
+
+export async function translate<T extends TranslateTextParameter>(texts: T) {
+  // const targetLanguages = await translator.getTargetLanguages();
+
+  const result = await translator.translateText(texts, "ja", "en-US");
+  return result;
+}
