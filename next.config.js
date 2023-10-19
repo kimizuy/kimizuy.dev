cb7ef67f8ff81e56e2be4881b6f93de13b589bfd
kimizuy <kimizuyamasaki@gmail.com>
2023-10-19 14:59:12 -0700

    refactor

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const OLD_BLOG_HOST = {
-  type: "host",
-  value: "blog.kimizuy.dev",
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,33 +6,48 @@ const nextConfig = {
     // https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages
     serverComponentsExternalPackages: ["budoux"],
   },
-  // redirect "blog.kimizuy.dev" to "kimizuy.dev/blog"
-  async redirects() {
-    return [
-      {
-        source: "/",
-        has: [OLD_BLOG_HOST],
-        destination: "https://kimizuy.dev/blog",
-        permanent: true,
-      },
-    ];
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/posts/:path*",
-          has: [OLD_BLOG_HOST],
-          destination: "https://kimizuy.dev/blog/post/:path*",
-        },
-        {
-          source: "/tags/:path*",
-          has: [OLD_BLOG_HOST],
-          destination: "https://kimizuy.dev/blog/tag/:path*",
-        },
-      ],
-    };
-  },
+  ...redirectBlog(),
 };
 
 module.exports = nextConfig;
+
+/**
+ * Redirect `blog.kimizuy.dev` to `kimizuy.dev/blog`
+ */
+function redirectBlog() {
+  const OLD_BLOG_HOST = {
+    type: "host",
+    value: "blog.kimizuy.dev",
+  };
+  /** @type {import('next').NextConfig} */
+  const config = {
+    async redirects() {
+      return [
+        {
+          source: "/",
+          has: [OLD_BLOG_HOST],
+          destination: "https://kimizuy.dev/blog",
+          permanent: true,
+        },
+      ];
+    },
+    async rewrites() {
+      return {
+        beforeFiles: [
+          {
+            source: "/posts/:path*",
+            has: [OLD_BLOG_HOST],
+            destination: "https://kimizuy.dev/blog/post/:path*",
+          },
+          {
+            source: "/tags/:path*",
+            has: [OLD_BLOG_HOST],
+            destination: "https://kimizuy.dev/blog/tag/:path*",
+          },
+        ],
+      };
+    },
+  };
+
+  return config;
+}
