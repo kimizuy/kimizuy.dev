90e4d707d4b801eeabe09638a008704b7bd47290
kimizuy <kimizuyamasaki@gmail.com>
2023-10-19 19:40:38 -0700

    add packages for merging classnames

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,6 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function dateSortDesc(a: Date, b: Date): number {
   if (a > b) return -1;
   if (a < b) return 1;
@@ -37,4 +40,8 @@ function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
 
 export function getErrorMessage(error: unknown) {
   return toErrorWithMessage(error).message;
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
