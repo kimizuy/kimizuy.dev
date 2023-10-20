75df7500125b343d15d779f80cb030564ce2b861
kimizuy <kimizuyamasaki@gmail.com>
2023-10-19 17:12:45 -0700

    refactor

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,11 @@
 import { CardList } from "../../components/card-list";
 import { ContentLayout } from "../../components/content-layout";
 import { TagList } from "../../components/tag-list";
-import { getAllPosts } from "../../utils/post";
+import { getAllPosts, getAllTags } from "../../utils/post";
 
 export default async function Page() {
   const posts = await getAllPosts();
-  const tags = [
-    ...new Set(
-      posts.map((post) => post.frontmatter.tags).flatMap((tag) => tag),
-    ),
-  ];
+  const tags = await getAllTags();
 
   return (
     <ContentLayout
