f0af9984b1ca65be8fac53256681aab740822356
kimizuy <kimizuyamasaki@gmail.com>
2023-10-20 18:47:15 -0700

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,39 @@
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 const config = {
-  extends: ["next/core-web-vitals", "plugin:mdx/recommended"],
+  extends: [
+    "plugin:mdx/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "unused-imports", "simple-import-sort"],
+  root: true,
   rules: {
     "no-console": [
       "warn",
       {
         allow: ["warn", "error"],
       },
+    ],
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      // remove newline between import groups
+      { groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]] },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
     ],
   },
   settings: {
