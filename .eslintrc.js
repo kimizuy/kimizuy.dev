34a5da1c08cc77243d73540d6f7ee5005685b384
kimizuy <kimizuyamasaki@gmail.com>
2023-10-23 17:04:16 -0700

    ignore .md & .mdx files

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,7 +59,7 @@ const config = {
     "mdx/code-blocks": true,
     "mdx/language-mapper": {},
   },
-  ignorePatterns: ["type-predicates.ts", ".eslintrc.js"],
+  ignorePatterns: ["type-predicates.ts", ".eslintrc.js", "*.md", "*.mdx"],
 };
 
 module.exports = config;
