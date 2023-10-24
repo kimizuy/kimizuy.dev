b291e081f594aded629b52b2052bcdcc1274be34
kimizuy <kimizuyamasaki@gmail.com>
2023-10-23 17:05:59 -0700

    add md-to-pdf

---
+++ md-to-pdf.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  headless: true,
+  dest: "public/Kimizu Yamasaki - Resume.pdf",
+  document_title: "Kimizu Yamasaki - Resume",
+  pdf_options: {
+    margin: "20mm",
+  },
+  md_file_encoding: "utf-8",
+};
