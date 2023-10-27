9622ffedbe201c16174f389eb571aed1a09cf82e
kimizuy <kimizuyamasaki@gmail.com>
2023-10-26 18:34:40 -0700

    update styles for toc

--- src/libs/toc.tsx
+++ src/libs/toc.tsx
@@ -3,14 +3,11 @@
 import "./toc.css";
 import { useEffect } from "react";
 import tocbot from "tocbot";
-import { useWindowWidth } from "../utils/use-window-width";
 
 type Props = { headingSelector?: string };
 
 export function Toc({ headingSelector }: Props) {
-  const { width } = useWindowWidth();
-  const isMobile = width <= 768;
-  const offset = isMobile ? 32 : 48;
+  const offset = 10 * 16;
 
   useEffect(() => {
     tocbot.init({
@@ -18,6 +15,7 @@ export function Toc({ headingSelector }: Props) {
       contentSelector: ".toc-content",
       headingSelector: headingSelector ?? "h2, h3, h4",
       orderedList: false,
+      scrollSmooth: false,
       // ref: http://tscanlin.github.io/tocbot/#fixed-headers
       headingsOffset: offset,
       scrollSmoothOffset: -1 * offset,
